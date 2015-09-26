066347d389e99f51536ecc87070aa34d5dedc7bf
jlopez <jahlopez81@gmail.com>
2015-09-26 00:11:23 -0400

    Updated jquery.ajaxForm.js
    added ability to trigger ajax submit on select change.

--- jquery.ajaxForm.js
+++ jquery.ajaxForm.js
@@ -1,64 +1,45 @@
-;(function ($) {
-	'use strict';
-	$.fn.ajaxForm = function (options) {
-		var isForm = (this.prop('tagName') === 'FORM');
+(function ($) {
+    'use strict';
+    var getFormData = function (element) {
+        var $form = $(element),
+            tag = element.tagName,
+            namespace = '.ajaxForm',
+            event;
 
-		return ({
+        if (tag === 'FORM') {
+            event = 'submit';
+        } else {
+            event = (tag === 'SELECT') ? 'change' : 'click';
+            $form = $form.closest('form');
+        }
 
-			init: function ($elements, options) {
-				if ($elements.length) {
-					var eventType = (isForm) ? 'submit.ajaxForm' : 'click.ajaxForm',
-						settings = this.validate(options);
+        event += namespace;
 
-					return $elements.off('.ajaxForm').on(eventType, {
-						options: settings
-					}, this.eventHandler.bind(this));
-				}
-			},
+        return {
+            event: event,
+            namespace: namespace,
+            getOptions: function () {
+                var enctype = $form.prop('enctype') || 'application/x-www-form-urlencoded',
+                    charset = $form.prop('accept-charset') || 'UTF-8';
+                return {
+                    contentType: enctype + '; charset=' + charset,
+                    method: $form.prop('method') || 'GET',
+                    url: $form.prop('action') || '',
+                    data: $form.serialize() || ''
+                };
+            }
+        };
+    };
 
-			validate: function (options) {
-				var settings = {};
+    $.fn.ajaxForm = function (options) {
+        return this.each(function () {
+            var form = getFormData(this);
+            $(this).off(form.namespace).on(form.event, function (e) {
+                e.preventDefault();
+                var settings = $.extend(form.getOptions(), options);
+                $.ajax(settings);
+            });
+        });
+    };
 
-				try {
-					if (typeof options === 'object') {
-						settings = options;
-					} else if (typeof options === 'function') {
-						settings.success = options;
-					} else {
-						throw new Error(options + ' is not a valid object or function. Please make sure pass a valid argument.');
-					}
-
-				} catch (e) {
-					console.log('$.ajaxForm()', e.message);
-
-				} finally {
-					return settings;
-				}
-			},
-
-			eventHandler: function (e) {
-				e.preventDefault();
-				var formData = this.getFormData(e.currentTarget) || {},
-					settings = $.extend({}, formData, e.data.options) || {};
-
-				$.ajax(settings);
-			},
-
-			getFormData: function (element) {
-				var $form = (isForm) ? $(element) : $(element).closest('form'),
-					charset = $form.prop('accept-charset') || 'UTF-8',
-					enctype = $form.prop('enctype') || 'application/x-www-form-urlencoded';
-
-				return {
-					contentType: enctype + '; charset=' + charset,
-					method: $form.prop('method') || 'GET',
-					url: $form.prop('action') || '',
-					data: $form.serialize() || ''
-				};
-			}
-
-		}.init(this, options));
-		
-	};
-	
 }(jQuery));
